29b0fb0b1603b6eabcb294cf49714a1aaa8387c7
Ben Botwin <benj@minbot.win>
2024-11-09 11:44:53 -0500

    Camera switching works now

--- camera/camera.gd
+++ camera/camera.gd
@@ -1,11 +1,39 @@
-extends Camera3D
+extends Node3D
+class_name Camera
 
-@export var mouse_sensitivity = 0.01
+@onready var camera: Camera3D = $"./pivot/Camera3D"
+@onready var pivot: Node3D = $"./pivot"
+@onready var cast: ShapeCast3D = $"./pivot/Camera3D/StaticBody3D/ShapeCast3D"
 
-func _process(delta):
-	print(self.unproject_position(get_tree().root.get_child(0).find_child("sphere").global_position))
-	
+@export var mouse_sensitivity := 0.01
+
+func get_rel_screen_pos(object: Node3D) -> Vector2:
+	var screen_pos = camera.unproject_position(object.global_position)
+	var rel = screen_pos / get_viewport().get_visible_rect().size - Vector2(0.5, 0.5)
+	return rel
+
+func _physics_process(_delta: float) -> void:
+	if not camera.current:
+		return
+
+	if Input.is_action_just_pressed("fire"):
+		if cast.is_colliding():
+			var body = cast.get_collider(0)
+			var maybe_cam = body
+
+			while not maybe_cam is Camera and maybe_cam != null:
+				maybe_cam = maybe_cam.get_parent()
+
+			if maybe_cam != null:
+				maybe_cam.call_deferred("player_activate")
+
+func player_activate() -> void:
+	camera.current = true
+	print(camera.current)
+
 func _unhandled_input(event):
+	if not camera.current:
+		return
 	if event is InputEventKey:
 		if event.keycode == KEY_Q:
 			get_tree().quit()
@@ -15,7 +43,7 @@ func _unhandled_input(event):
 			else:
 				Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
-		var x = event.relative.x
-		var y = event.relative.y
-		rotate_x(-y * mouse_sensitivity)
-		$"..".rotate_y(-x * mouse_sensitivity)
+		var x = event.relative.x if event.relative.x else 0
+		var y = event.relative.y if event.relative.x else 0
+		camera.rotate_x(-y * mouse_sensitivity)
+		pivot.rotate_y(-x * mouse_sensitivity)
